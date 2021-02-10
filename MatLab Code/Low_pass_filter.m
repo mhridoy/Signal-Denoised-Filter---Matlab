c14f1a24bc3a33634732e9207af2e476a7253421
Md.Moshiur Rahman <35825505+mhridoy@users.noreply.github.com>
2021-02-10 14:26:07 +0600

    Update Low_pass_filter.m

--- MatLab Code/Low_pass_filter.m
+++ MatLab Code/Low_pass_filter.m
@@ -1,2 +1,7 @@
 dataset = importdata('signal.txt');
-pspectrum(dataset)
+lpFilt = designfilt('lowpassfir','PassbandFrequency',0.25, ...
+         'StopbandFrequency',0.35,'PassbandRipple',0.5, ...
+         'StopbandAttenuation',65,'DesignMethod','kaiserwin');
+fvtool(lpFilt)
+
+dataOut = filter(lpFilt,dataset);
